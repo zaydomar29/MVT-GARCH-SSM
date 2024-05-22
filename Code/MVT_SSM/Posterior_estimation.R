df1bfd77e189b6919d9c52f334370916ad8227b2
zaydomar29 <zaydomar29@gmail.com>
2024-05-22 10:32:21 -0700

    Create Posterior_estimation.R

---
+++ Code/MVT_SSM/Posterior_estimation.R
@@ -0,0 +1,87 @@
+##################################################################
+##################################################################
+##############                               #####################
+##############       A Gibbs Sampler         #####################
+##############                               #####################
+##################################################################
+##################################################################
+Rcpp::sourceCpp("./Functions/MvtKalFilt.cpp")
+Rcpp::sourceCpp("./Functions/FFBS.cpp")
+library(MCMCpack)
+library(mvtnorm)
+
+
+# We use Inverse-Wishart Priors for V and W
+k=4; p=4
+
+V.sample = list(diag(50,k))
+W.sample = list(diag(0.1,p))
+S.y = diag(10,k); n.y = 10;
+S.t = diag(10,p); n.t = 10;
+
+nsims = 5000
+burn = 1500
+thin = 2
+est = seq(burn+1,nsims,thin)
+
+m0 = Y[1,]
+C0 = diag(100,p)
+
+predicted = list()
+sampled_theta = list()
+
+for(i in 2:nsims){
+  if(i %% 100 == 0){print(i)} 
+  # if(i %% 500 == 0){print(V.sample[[i-1]])}
+  # if(i %% 500 == 0){print(W.sample[[i-1]])}
+  
+  
+  n = length(Y[,1])
+  out = MVTKalFiltC(Y,GG,FF,V.sample[[i-1]],W.sample[[i-1]],m0,C0)
+  theta = do.call(rbind, lapply(ffbsC(Y,out)$theta,t))
+  
+  if(round((i-burn-1)/thin)==((i-burn-1)/thin) & (i-burn-1) > 0 ){
+    sampled_theta[[round((i-burn-1)/thin)]] = theta
+    predicted[[round((i-burn-1)/thin)]] = do.call(rbind, lapply(out$y_t_1,t))
+  }
+  
+  row.na = sort(which(is.na(Y),arr.ind = T)[,1])
+  if(length(row.na) != 0){
+    SS.y = crossprod((Y-t(FF%*%t(theta[-1,])))[-row.na,]) + S.y
+  }else{
+    SS.y = crossprod((Y-t(FF%*%t(theta[-1,])))) + S.y
+  }
+  
+  
+  # SS.y = crossprod((Y-theta[-1,])) + S.y
+  df.y = n+n.y
+  V.sample[[i]] = riwish(df.y,SS.y)
+  
+  SS.t = crossprod(theta[-1,]-t(GG%*%t(theta[-n,])) ) + S.t
+  df.t = n+n.t
+  W.sample[[i]] = riwish(df.t,SS.t)
+
+
+}
+
+
+
+
+
+
+
+
+
+
+##################################################################
+##################################################################
+##############                               #####################
+##############       WAIC for MVT mod        #####################
+##############                               #####################
+##################################################################
+##################################################################
+Rcpp::sourceCpp("Functions/waic1_std.cpp")
+
+waic1 = MVT_waic1(V.sample, W.sample, Y,mode = "Std",FF = FF, GG=GG,m0=m0,C0=C0)
+waic1
+
