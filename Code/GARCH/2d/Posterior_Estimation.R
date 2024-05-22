86faa7b81a132ae4e4a6f25a42d77e0c47f6fd78
zaydomar29 <zaydomar29@gmail.com>
2024-05-22 12:26:09 -0700

    Update Posterior_Estimation.R

--- Code/GARCH/2d/Posterior_Estimation.R
+++ Code/GARCH/2d/Posterior_Estimation.R
@@ -5,9 +5,9 @@
 ##############                               #####################
 ##################################################################
 ##################################################################
-Rcpp::sourceCpp("./Functions/MvtKalFiltGarch.cpp")
-Rcpp::sourceCpp("./Functions/MvtKalFFBS.cpp")
-Rcpp::sourceCpp("./Functions/MvtLogPost.cpp")
+Rcpp::sourceCpp("./Functions/BvtKalFiltGarch.cpp")
+Rcpp::sourceCpp("./Functions/BvtKalFFBS.cpp")
+Rcpp::sourceCpp("./Functions/BvtLogPost.cpp")
 Rcpp::sourceCpp("./Functions/diwishart.cpp")
 library(MCMCpack)
 library(truncnorm)
@@ -41,12 +41,12 @@ old_param = rbind(c(1,0.2,0.1),
 rho.old = 0.0
 W.old = diag(c(1.5,2.5))
 
-out = MVTKalFiltGarchC(Y=Y,GG=GG,FF=FF,W=W.old,m0=Y[1,],C0=C0,garch=old_param,cor=rho.old)
-theta = do.call(rbind,lapply(MVTKalffbsC(Y, out)$theta,t))
+out = BvtKalFiltGarchC(Y=Y,GG=GG,FF=FF,W=W.old,m0=Y[1,],C0=C0,garch=old_param,cor=rho.old)
+theta = do.call(rbind,lapply(BvtKalffbsC(Y, out)$theta,t))
 
 
 
-old_lgpost = MVT_lgpostSS(par = old_param,cor = rho.old,W = W.old,Y = Y, theta = theta, nt = 10, psi = diag(10,2))
+old_lgpost = BvTLogPost(par = old_param,cor = rho.old,W = W.old,Y = Y, theta = theta, nt = 10, psi = diag(10,2))
 
 acc.rate.a0 = c(0,0)
 acc.rate.a1 = c(0,0)
@@ -79,9 +79,9 @@ for(it in 1:sims){
     a0.prop = rtruncnorm(1,a=0,b=Inf,mean=old_param[j,1],1)
     new_param = old_param
     new_param[j,1] = a0.prop
-    rho = MVT_lgpostSS(par = new_param,cor = rho.old,W = W.old,Y = Y, theta = theta, nt = 10, psi = diag(10,2))+
+    rho = BvTLogPost(par = new_param,cor = rho.old,W = W.old,Y = Y, theta = theta, nt = 10, psi = diag(10,2))+
       log(dtruncnorm(old_param[j,1],a=0,b=Inf,mean=a0.prop,0.1))-
-      MVT_lgpostSS(par = old_param,cor = rho.old,W = W.old,Y = Y, theta = theta, nt = 10, psi = diag(10,2))-
+      BvTLogPost(par = old_param,cor = rho.old,W = W.old,Y = Y, theta = theta, nt = 10, psi = diag(10,2))-
       log(dtruncnorm(a0.prop,a=0,b=Inf,mean=old_param[j,1],0.1))
     
     
@@ -96,9 +96,9 @@ for(it in 1:sims){
     a1.prop = rtruncnorm(1,a=0,b=1-old_param[j,3],mean=old_param[j,2],0.01)
     new_param = old_param
     new_param[j,2] = a1.prop
-    rho = MVT_lgpostSS(par = new_param,cor = rho.old,W = W.old,Y = Y, theta = theta, nt = 10, psi = diag(10,2))+
+    rho = BvTLogPost(par = new_param,cor = rho.old,W = W.old,Y = Y, theta = theta, nt = 10, psi = diag(10,2))+
       log(dtruncnorm(old_param[j,2],a=0,b=1-old_param[j,3],mean=a1.prop,0.01))-
-      MVT_lgpostSS(par = old_param,cor = rho.old,W = W.old,Y = Y, theta = theta, nt = 10, psi = diag(10,2))-
+      BvTLogPost(par = old_param,cor = rho.old,W = W.old,Y = Y, theta = theta, nt = 10, psi = diag(10,2))-
       log(dtruncnorm(a1.prop,a=0,b=1-old_param[j,3],mean=old_param[j,2],0.01))
     
     
@@ -113,9 +113,9 @@ for(it in 1:sims){
     b1.prop = rtruncnorm(1,a=0,b=1-old_param[j,2],mean=old_param[j,3],0.01)
     new_param = old_param
     new_param[j,3] = b1.prop
-    rho = MVT_lgpostSS(par = new_param,cor = rho.old,W = W.old,Y = Y, theta = theta, nt = 10, psi = diag(10,2))+
+    rho = BvTLogPost(par = new_param,cor = rho.old,W = W.old,Y = Y, theta = theta, nt = 10, psi = diag(10,2))+
       log(dtruncnorm(old_param[j,3],a=0,b=1-old_param[j,2],mean=b1.prop,0.01))-
-      MVT_lgpostSS(par = old_param,cor = rho.old,W = W.old,Y = Y, theta = theta, nt = 10, psi = diag(10,2))-
+      BvTLogPost(par = old_param,cor = rho.old,W = W.old,Y = Y, theta = theta, nt = 10, psi = diag(10,2))-
       log(dtruncnorm(b1.prop,a=0,b=1-old_param[j,2],mean=old_param[j,3],0.01))
     
     
@@ -143,8 +143,8 @@ for(it in 1:sims){
     rho.prop = rho.old+runif(1,-0.1,0.1)
   }
   
-  rho = MVT_lgpostSS(par = old_param,cor = rho.prop,W = W.old,Y = Y, theta = theta, nt = 10, psi = diag(1,2))-
-        MVT_lgpostSS(par = old_param,cor = rho.old,W = W.old,Y = Y, theta = theta, nt = 10, psi = diag(1,2))
+  rho = BvTLogPost(par = old_param,cor = rho.prop,W = W.old,Y = Y, theta = theta, nt = 10, psi = diag(1,2))-
+        BvTLogPost(par = old_param,cor = rho.old,W = W.old,Y = Y, theta = theta, nt = 10, psi = diag(1,2))
 
   if(log(runif(1)) < rho){
     rho.old = rho.prop
@@ -157,8 +157,8 @@ for(it in 1:sims){
     }
   }
 
-  out = MVTKalFiltGarchC(Y=Y,GG=GG,FF=FF,W=W.old,m0=Y[1,],C0=C0,garch=old_param,cor=rho.old)
-  theta = do.call(rbind,lapply(MVTKalffbsC(Y, out)$theta,t))
+  out = BvtKalFiltGarchC(Y=Y,GG=GG,FF=FF,W=W.old,m0=Y[1,],C0=C0,garch=old_param,cor=rho.old)
+  theta = do.call(rbind,lapply(BvtKalffbsC(Y, out)$theta,t))
   
   if((it-burn-1)%%thin == 0 & (it-burn-1)/thin > 0 ){
     var1[[(it-burn-1)/thin]] = do.call(c,lapply(out$Q_t, function(x) x[1,1]))
